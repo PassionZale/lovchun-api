f36f4f4a07d7e5696a0bfe25ef0559c6cf7958f8
PassionZale <whouu@qq.com>
2021-01-16 21:16:48 +0800

    update: ormconfig.json 创建逻辑，typeorm migrate 脚本

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as moment from 'moment-timezone';
+
+import { AppModule } from './app.module';
+import { TypeormConfigService } from './config/typeorm/config.service';
+import { updateOrmConfigFileSync } from './common/helper/typeorm.helper';
+
 
 /**
  * 设置默认时区为东八区
@@ -10,6 +14,13 @@ moment.tz.setDefault('Asia/Shanghai');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 读取 TypeormConfig
+  const typeormConfig: TypeormConfigService = app.get(TypeormConfigService);
+
+  // 根据 TypeormConfig 创建 ormconfig.json
+  updateOrmConfigFileSync(typeormConfig.configs);
+
   await app.listen(3000);
 }
 
